9d6ba3dc472f3660c847e4df256f701184e528d1
yan <yannmarcel43@gmail.com>
2024-08-25 18:56:12 +0200

    refactoring : global clean architecture

--- lib/data/data_sources/chat/socket/message_socket_data_source_impl.dart
+++ lib/data/data_sources/chat/socket/message_socket_data_source_impl.dart
@@ -1,41 +1,52 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:my_app/utils/errors/handlers/socket_error_handler.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:web_socket_channel/status.dart' as status;
 import '../../../models/chat/socket/message_socket_model.dart';
 import 'message_socket_data_source.dart';
 
-
 class MessageSocketDataSourceImpl implements MessageSocketDataSource {
   WebSocketChannel? _channel;
   final StreamController<MessageSocketModel> _controller = StreamController<MessageSocketModel>.broadcast();
+  bool _isConnected = false; // To track the connection state
 
   @override
   Future<void> connect(String roomId) async {
+    if (_isConnected) return; // Prevent multiple connections
+
     try {
       final String? socketUrl = dotenv.env['SOCKET_URL'];
       if (socketUrl == null || socketUrl.isEmpty) {
-        throw Exception('Socket URL is not defined in environment variables.');
+        throw const SocketErrorHandler('Socket URL is not defined in environment variables.');
       }
 
       final uri = Uri.tryParse('$socketUrl/ws?id=$roomId');
       if (uri == null) {
-        throw Exception('Invalid Socket URL: $socketUrl/ws?id=$roomId');
+        throw SocketErrorHandler('Invalid Socket URL: $socketUrl/ws?id=$roomId');
       }
 
       _channel = WebSocketChannel.connect(uri);
+      _isConnected = true;
+
       _channel!.stream.listen(
             (event) {
+          if (_controller.isClosed) return; // Check if the controller is closed
           final message = MessageSocketModel.fromJson(json.decode(event));
           _controller.add(message);
         },
         onError: (error) {
-          _controller.addError(error);
-          disconnect();
+          if (!_controller.isClosed) {
+            _controller.addError(error);
+          }
+          disconnect(); // Disconnect on error
         },
         onDone: () {
-          _controller.close();
+          if (!_controller.isClosed) {
+            _controller.close(); // Close the controller only if it's not already closed
+          }
+          _isConnected = false; // Update connection state
         },
         cancelOnError: true,
       );
@@ -44,7 +55,7 @@ class MessageSocketDataSourceImpl implements MessageSocketDataSource {
     }
   }
 
-  @override 
+  @override
   void sendMessage(MessageSocketModel message) {
     if (_channel == null) {
       throw Exception('WebSocket connection is not established.');
@@ -53,7 +64,7 @@ class MessageSocketDataSourceImpl implements MessageSocketDataSource {
       final encodedMessage = json.encode(message.toJson());
       _channel!.sink.add(encodedMessage);
     } catch (e) {
-      throw Exception('Failed to send message: ${e.toString()}');
+      throw SocketErrorHandler('Failed to send message: ${e.toString()}');
     }
   }
 
@@ -62,7 +73,13 @@ class MessageSocketDataSourceImpl implements MessageSocketDataSource {
 
   @override
   Future<void> disconnect() async {
+    if (!_isConnected) return; // Only disconnect if currently connected
+    _isConnected = false;
     await _channel?.sink.close(status.normalClosure);
     _channel = null;
+
+    if (!_controller.isClosed) {
+      await _controller.close(); // Ensure controller is closed only if it's not already closed
+    }
   }
 }
